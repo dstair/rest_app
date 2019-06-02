d0dc487acdfb353b6f10f6936cc9b9435f42da93
Daniel S <daniel@dmbdata.com>
2019-06-02 08:17:23 -0400

    PUT requests return JSON with auto generated ID

--- venv/app/server.py
+++ venv/app/server.py
@@ -28,24 +28,35 @@ class People(Resource):
       return "Encountered an error. Are you sure the person ID you requested exists?"
 
   def delete(self, person_id):
-    #try:
+    try:
       person_json = return_person_json(person_id)
       conn = db_connect.connect()
       query = conn.execute("DELETE FROM people WHERE id = %s;" %str(person_id))
       return person_json
-    #except Exception:
-    #  return "Failed to delete person with ID %s, are you sure this ID exists?".format(person_id)
+    except Exception:
+      return "Failed to delete person with ID %s, are you sure this ID exists?".format(person_id)
 
-  # Note: using PUT instead of POST here. This is because each class can only have 1 associated route.
-  #  I chose to have all 3 actions (get/delete/put) in 1 class because that is clearer to me.
-  def put(self, person_id):
+class NewPeople(Resource):
+  def put(self):
     try:
       conn = db_connect.connect()
       new_person_dict = json.loads(request.form['data'])
-      query = conn.execute("INSERT INTO people (id, name, age, locale) \
-                            VALUES ('{}', '{}', {}, '{}');".format(new_person_dict['id'], new_person_dict['name'], new_person_dict['age'], new_person_dict['locale']) )
-      return "Successfully created person with ID {}".format(new_person_dict['id'])
+      query = conn.execute("INSERT INTO people (name, age, locale) \
+                            VALUES ('{}', {}, '{}');".format(new_person_dict['name'], new_person_dict['age'], new_person_dict['locale']) )
+      # Note: last_insert_rowid returns the rowid for the latest insert from this connection,
+      #  so using it will not result in concurrency issues.
+      query_inserted_id = conn.execute("select last_insert_rowid();")
+      new_person_id = query_inserted_id.fetchall()[0][0]
+      return json.jsonify(id=new_person_id,
+                    name=new_person_dict['name'],
+                    age=new_person_dict['age'],
+                    locale=new_person_dict['locale'])
     except Exception:
-      return "Failed to create person with ID {}".format(str(new_person_dict['id']))
+      return "Failed to create person with Name".format(new_person_dict['name'])
 
 api.add_resource(People, '/resources/data/<string:person_id>')
+api.add_resource(NewPeople, '/resources/data/')
+
+
+
+
